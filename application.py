137af73747bb014d1772fd9ab6335fcb4fe9a38d
Tiche12 <stshimanga1@ivytech.edu>
2024-04-20 17:41:33 -0400

    Application.py

---
+++ application.py
@@ -0,0 +1,31 @@
+from flask import Flask
+app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String(80), unique=True, nullable=False)
+    author = db.Column(db.String(120), unique=False)
+    publisher = db.Column(db.String(140), unique=False)
+
+
+    def __repr(self):
+        return f"{self.book_name} - {self.author} - {self.publisher}"
+
+
+
+
+
+@app.route('/books')
+def get_books():
+    books = Book.query.all()
+    output = []
+    for book in books:
+        book_data = {'name': book.book_name, 'author' : book.author, 'publisher' : book.publisher}
+        output.append(book_data)
+
+    return{"books": output} 
